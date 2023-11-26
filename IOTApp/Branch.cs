a80b226da0a1af50d49035280687bf396d6a6e72
Emily Boegheim <20103559@tafe.wa.edu.au>
2023-11-26 19:20:44 +0800

    Add basic Branch class to represent the company branches.

---
+++ IOTApp/Branch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTApp
+{
+    /// <summary>
+    /// A branch of the company.
+    /// </summary>
+    class Branch
+    {
+        /// <summary>
+        /// The unique ID number of the branch.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// The name of the branch.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Initialise the branch with the given data.
+        /// </summary>
+        /// <param name="id">The unique ID number of the branch.</param>
+        /// <param name="name">The name of the branch.</param>
+        public Branch(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        /// <summary>
+        /// Represent the branch as a string by simply returning its name.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
